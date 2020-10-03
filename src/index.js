d8b1559211ebcb299cca261857f54ec191b6120f
Teufelex <gloomycoke@gmail.com>
2020-10-03 23:12:05 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{1,10}/g);
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '**********') {
+            res.push(' ');
+        } else {
+            let arr2 = arr[i].match(/.{1,2}/g);
+            for (let j = 0; j < arr.length; j++) {
+            if (arr2[j] == '00') {
+                arr2.splice(j, 1);
+                j--;
+            }
+            if (arr2[j] == '10') arr2.splice(j, 1, '.');
+            if (arr2[j] == '11') arr2.splice(j, 1, '-');
+           }
+           let a = arr2.join('');
+           res.push(a);
+        }
+    }
+    let sum = '';
+    for (let i = 0; i < res.length; i++) {
+        if (res[i] in MORSE_TABLE) {
+            sum += MORSE_TABLE[res[i]];
+        } else {
+            sum += ' ';
+        }
+    }
+    return sum;
 }
 
 module.exports = {
